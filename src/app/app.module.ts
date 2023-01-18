a35272ee16ce273f4cc8be80f8b1d873ecdb1a8e
Janis <janis.meister87@gmail.com>
2023-01-18 10:00:45 +0100

    added header as material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,43 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppComponent} from './app.component';
-import {HttpClientModule} from "@angular/common/http";
+import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatDialogModule} from "@angular/material/dialog";
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
-import {MatButtonModule} from "@angular/material/button";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
 import { EmployeeEditDialogComponent } from './employee-edit-dialog/employee-edit-dialog.component';
 import { EmployeeCreateDialogComponent } from './employee-create-dialog/employee-create-dialog.component';
-import {FormsModule} from "@angular/forms";
-import {MatTableModule} from "@angular/material/table";
-import {MatIconModule} from "@angular/material/icon";
-import { HeaderComponent } from './header/header.component';
-
+import { FormsModule } from "@angular/forms";
+import { MatTableModule } from "@angular/material/table";
+import { MatIconModule } from "@angular/material/icon";
+import { HeaderComponent } from './header/header.component'; import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
-  declarations: [	
+  declarations: [
     AppComponent,
     EmployeeListComponent,
     EmployeeEditDialogComponent,
     EmployeeCreateDialogComponent,
-      HeaderComponent
-   ],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatDialogModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatButtonModule,
-        FormsModule,
-        MatTableModule,
-        MatIconModule
-    ],
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    MatTableModule,
+    MatIconModule,
+    MatToolbarModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
