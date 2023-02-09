d1e4814e6bf966fdc1d8967c27c635a2e70ddce5
Nils <40796807+sonsan@users.noreply.github.com>
2023-02-09 17:05:48 +0100

    documentation of service methods

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,65 +2,117 @@ import { Injectable } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { Employee } from './Employee';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Skill } from "./Skill";
+import { Skill } from './Skill';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  // method to add an employee
+  /**
+   * Adds a employee to the backend.
+   * @param {Employee} employee the employee to add.
+   * @return {Observable<Employee>} the changed object.
+   */
   addEmployee(employee: Employee): Observable<Employee> {
     console.log(`Adding ${JSON.stringify(employee)} to backend`);
 
     if (employee.skillSet == undefined) {
-      employee.skillSet = [""];
+      employee.skillSet = [''];
     }
-    return this.http.post<Employee>("/backend/employees", employee, {
-      headers: this.getHeaders()
-    });
-  }
-
-  // method to update an employee
-  updateEmployee(employee: Employee): Observable<Employee> {
-    console.log(`Updating ${JSON.stringify(employee)}`);
-    return this.http.put<Employee>(`/backend/employees/${employee.id}`, employee, {
+    return this.http.post<Employee>('/backend/employees', employee, {
       headers: this.getHeaders(),
     });
   }
 
-  // method to delete an employee
+  /**
+   * Updates a existing employee.
+   * @param {Employee} employee the employee to update.
+   * @return {Observable<Employee>} the changed object.
+   *
+   * Will change the existing employee with the input parameter.
+   * Based on the employee id.
+   */
+  updateEmployee(employee: Employee): Observable<Employee> {
+    console.log(`Updating ${JSON.stringify(employee)}`);
+    return this.http.put<Employee>(
+      `/backend/employees/${employee.id}`,
+      employee,
+      {
+        headers: this.getHeaders(),
+      }
+    );
+  }
+
+  /**
+   * Delete a employee.
+   * @param {number} employeeId the id of the employee to delete.
+   *
+   * Will delete a existing employee based on id.
+   */
   deleteEmployee(employeeId: number): Observable<any> {
     return this.http.delete<any>(`/backend/employees/${employeeId}`, {
       headers: this.getHeaders(),
     });
   }
 
-  // method to get all employees
+  /**
+   * Read all employees from the Backend.
+   */
   getAllEmployees(): Observable<Employee[]> {
-    console.log('loading employees from backend')
+    console.log('loading employees from backend');
     return this.http.get<Employee[]>('/backend/employees', {
       headers: this.getHeaders(),
     });
   }
 
-  addEmployeeQualification(employee: Employee, skill: Skill): Observable<Employee> {
-    return this.http.post<Employee>(`/backend/employees/${employee.id}/`, skill, {
-      headers: this.getHeaders()
-    });
+  /**
+   * Add a qualification for the employee.
+   * @param {Employee} employee the employee to give the qualification
+   * @param {Skill} skill the qualification to give the employee.
+   * @note Currently not supported by the backend.
+   */
+  addEmployeeQualification(
+    employee: Employee,
+    skill: Skill
+  ): Observable<Employee> {
+    return this.http.post<Employee>(
+      `/backend/employees/${employee.id}/`,
+      skill,
+      {
+        headers: this.getHeaders(),
+      }
+    );
   }
 
+  /**
+   * Remove the skill from the backend.
+   * @param {Skill} skill the skill to delete.
+   */
   removeEmployeeQualification(skill: Skill): Observable<Employee> {
-    return this.http.delete<Employee>(`/backend/employees/${skill.designation}/qualifications`, {
-      headers: this.getHeaders()
-    });
+    return this.http.delete<Employee>(
+      `/backend/employees/${skill.designation}/qualifications`,
+      {
+        headers: this.getHeaders(),
+      }
+    );
   }
 
+  /**
+   * Get all employees who have a specified skill.
+   * @param {Skill} skill the skill with which to search.
+   * @returns {Employee} list of all employees with the skill.
+   */
   getEmployeesWithSkill(skill: Skill): Observable<Employee[]> {
-    return this.http.get<Employee[]>(`/backend/qualifications/${skill.designation}/employees`, {
-      headers: this.getHeaders()
-    }).pipe(map((resp: any) => resp.employees));
+    return this.http
+      .get<Employee[]>(
+        `/backend/qualifications/${skill.designation}/employees`,
+        {
+          headers: this.getHeaders(),
+        }
+      )
+      .pipe(map((resp: any) => resp.employees));
   }
 
   private getHeaders(): HttpHeaders {
--- src/app/skillset.service.ts
+++ src/app/skillset.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import {Skill} from "./Skill";
+import { Skill } from './Skill';
 
 @Injectable({
   providedIn: 'root',
@@ -9,24 +9,43 @@ import {Skill} from "./Skill";
 export class SkillsetService {
   constructor(private http: HttpClient) {}
 
-  // method to add an employee
+  /**
+   * Create a new skill.
+   * @param {Skill} skill the skill to create.
+   *
+   * Will add the skill to the backend.
+   */
   addSkill(skill: Skill): Observable<Skill> {
     console.log(`Adding ${JSON.stringify(skill)} to backend `);
-    return this.http.post<Skill>("/backend/qualifications", {designation: skill.designation}, {
-      headers: this.getHeaders()
-    });
+    return this.http.post<Skill>(
+      '/backend/qualifications',
+      { designation: skill.designation },
+      {
+        headers: this.getHeaders(),
+      }
+    );
   }
 
-  // method to delete an employee
+  /**
+   * Delete a skill.
+   * @param {Skill} skill the skill to delete.
+   *
+   * Will delete the skill from the backend based on it's designation.
+   */
   deleteSkill(skill: Skill): Observable<any> {
-    return this.http.delete<any>(`/backend/qualifications/${skill.designation}`, {
-      headers: this.getHeaders(),
-    });
+    return this.http.delete<any>(
+      `/backend/qualifications/${skill.designation}`,
+      {
+        headers: this.getHeaders(),
+      }
+    );
   }
 
-  // method to get all employees
+  /**
+   * Get all available skills.
+   */
   getAllSkills(): Observable<Skill[]> {
-    console.log('loading skills from backend')
+    console.log('loading skills from backend');
     return this.http.get<Skill[]>('/backend/qualifications', {
       headers: this.getHeaders(),
     });
